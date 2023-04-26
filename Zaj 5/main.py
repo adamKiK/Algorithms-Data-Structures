ca3074edd88aaaa74552e46f49b115c228e4f37d
Adam K <kozlik.adam.gis@gmail.com>
2023-04-26 13:10:42 +0200

    Huffman -> Correctly create tree

--- Zaj 5/main.py
+++ Zaj 5/main.py
@@ -16,20 +16,21 @@ def set_node_frequencies():
         node_array.append(node.Node(character))
         node_array[index].set_frequency(character_frequency)
 
+def set_nodes_least_frequency_indexes(node_array):
+    lowest_frequency_index = 0
+    lowest_value = node_array[lowest_frequency_index].return_frequency()
+    second_lowest_frequency_index = 1
 
+    for node_index in range(1, len(node_array)):
+        if node_array[node_index].return_frequency() <= lowest_value:
+            second_lowest_frequency_index = lowest_frequency_index
+            lowest_frequency_index = node_index
+            lowest_value = node_array[lowest_frequency_index].return_frequency()
 def __main__():
     set_node_frequencies()
 
     while len(node_array) > 1:
-        lowest_frequency_index = 0
-        lowest_value = node_array[lowest_frequency_index].return_frequency()
-        second_lowest_frequency_index = 1
-
-        for node_index in range(1, len(node_array)):
-            if node_array[node_index].return_frequency() <= lowest_value:
-                second_lowest_frequency_index = lowest_frequency_index
-                lowest_frequency_index = node_index
-                lowest_value = node_array[lowest_frequency_index].return_frequency()
+        set_nodes_least_frequency_indexes()
 
         node_array.append(node.Node())
         node_array[-1].append_child_nodes(node_array[lowest_frequency_index], node_array[second_lowest_frequency_index])
@@ -40,7 +41,6 @@ def __main__():
         else:
             del node_array[second_lowest_frequency_index]
             del node_array[lowest_frequency_index]
-
 
     return 0
 
