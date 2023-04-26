bcdc824c85ffc76b8d335576b275be741e6932f2
Adam K <kozlik.adam.gis@gmail.com>
2023-04-26 15:41:19 +0200

    Huffman -> Restructured, correctly encodes, decodes text

--- Zaj 5/main.py
+++ Zaj 5/main.py
@@ -5,8 +5,10 @@ def __main__():
     input_text = 'Hello World'
     huffman = hc.HuffmanCoding(input_text)
     huffman.create_huffman_tree()
-    top_tree_node = huffman.get_top_tree_node()
-    huffman.encode_tree(top_tree_node)
+    top_parent_node = huffman.get_top_tree_node()
+    huffman.encode_tree(top_parent_node)
+    print(huffman.encode_text())
+    print(huffman.decode_text())
     return 0
 
 
